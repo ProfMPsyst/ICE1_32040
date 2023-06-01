6efcfa93d52aef5b27c2e94427071c9340cf5d92
neha6709 <neha22@sheridancollege.ca>
2023-06-01 10:02:23 -0400

    hello from Neha

--- src/ice1/CardTrick.java
+++ src/ice1/CardTrick.java
@@ -8,7 +8,7 @@ package ice1;
 /**A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- *
+ * @modifier Neha
  * @author Megha Patel,2023
  */
 public class CardTrick {
