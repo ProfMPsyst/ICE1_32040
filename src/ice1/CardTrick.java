92782a1edd95d792a56211b82a3b41c5f2542ed8
jainil <pate3862@sheridancollege.ca>
2023-06-01 11:08:01 -0400

    code genrated for random numbers

--- src/ice1/CardTrick.java
+++ src/ice1/CardTrick.java
@@ -5,6 +5,8 @@
 
 package ice1;
 
+import java.util.Scanner;
+
 /**A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
@@ -24,13 +26,38 @@ public class CardTrick {
         for(int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue( (int) (Math.random() *13) +1);
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
+            magicHand[i] = c;           
         }
+            
+           
+        Scanner k = new Scanner(System.in);
+         System.out.print("Enter the cards from the deck ");
+        int userValue = k.nextInt();
+        System.out.print("Enter the card suit: ");
+        String userSuit = k.next();
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+        Card UC = new Card();
+        UC.setValue(userValue);
+        UC.setSuit(userSuit);
+        
+        boolean iscdfound = false;
+        for (Card crd : magicHand) {
+            if (crd.equals(UC)) {
+                iscdfound = true;
+                break;     
+        }
 
+        }
+        if (iscdfound) {
+            System.out.println("The card is in the magic hand.");
+        } else {
+            System.out.println("The card is not in the magic hand."); 
+        }
+        System.out.println("Magic Hand Cards:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }        
+    }    
 }
