9b6abc1325b3885c3a5f00cc6a9f41a501701d8f
saijasha <saijasha@sheridancollege.ca>
2023-06-01 11:14:09 -0400

    Modified CardTrick.java to ask user input and checks

--- src/ice1/CardTrick.java
+++ src/ice1/CardTrick.java
@@ -5,11 +5,16 @@
 
 package ice1;
 
+import java.io.ByteArrayInputStream;
+import java.util.Random;
+import java.util.Scanner;
+
 /**A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author Megha Patel,2023
+ * @modifier Jashanpreet Saini(saijasha) 991669648
  */
 public class CardTrick {
 
@@ -18,17 +23,30 @@ public class CardTrick {
      */
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for(int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        Random random = new Random();
+        Scanner s = new Scanner(System.in);
+
+        for (int i = 0; i < magicHand.length; i++) {
+            magicHand[i] = new Card();
+            int value = 1 + random.nextInt(13);
+            int suit = random.nextInt(4);
+            magicHand[i].setValue(value);
+            magicHand[i].setSuit(Card.SUITS[suit]);
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.print("Enter Suite - \"Hearts\", \"Diamonds\", \"Spades\", \"Clubs\": ");
+        String suite = s.nextLine();
+        
+        System.out.print("Enter card value: ");
+        int value = s.nextInt();
+        
+        for (int i = 0; i < magicHand.length; i++){
+            if (magicHand[i].getValue() == value && magicHand[i].getSuit() == suite.toLowerCase()) {
+                System.out.println("Congrats you guessed correctly");
+                break;
+            }
+        }
+        
     }
 
 }
