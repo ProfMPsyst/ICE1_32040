dbab7ba2ca5120baad85dc84dc6c76a88373f8b3
saijasha <saijasha@sheridancollege.ca>
2023-06-08 08:58:05 -0400

    Errors removed

--- src/ice1/CardTrick.java
+++ src/ice1/CardTrick.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package ice1;
 
 import java.util.Random;
 import java.util.Scanner;
 
-/**A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
  *
  * @author Megha Patel,2023
  * @modifier Jashanpreet Saini(saijasha) 991669648
@@ -24,11 +24,11 @@ public class CardTrick {
         Card[] magicHand = new Card[7];
         Random random = new Random();
         Scanner s = new Scanner(System.in);
-        
+
         // hard coding luckycard card object
         Card luckyCard = new Card();
-        luckyCard.setValue = 5;
-        luckyCard.setSuit = "hearts"
+        luckyCard.setValue(5);
+        luckyCard.setSuit("hearts");
 
         for (int i = 0; i < magicHand.length; i++) {
             magicHand[i] = new Card();
@@ -37,7 +37,7 @@ public class CardTrick {
             magicHand[i].setValue(value);
             magicHand[i].setSuit(Card.SUITS[suit]);
         }
-        
+
         /*System.out.print("Enter Suite - \"Hearts\", \"Diamonds\", \"Spades\", \"Clubs\": ");
         String suite = s.nextLine();
         
@@ -49,14 +49,12 @@ public class CardTrick {
                 System.out.println("Congrats you guessed correctly");
                 break;
             } */
-        
         for (int i = 0; i < magicHand.length; i++) {
-            if (magicHand[i].getValue() == luckyCard.getValue && magicHand[i].getSuit() == luckyCard.getSuit)
+            if (magicHand[i].getSuit().equals(luckyCard.getSuit()) && magicHand[i].getValue() == luckyCard.getValue()) {
                 System.out.println("Congrats you guessed correctly");
+            }
         }
-        
-        }
-        
+
     }
 
 }
